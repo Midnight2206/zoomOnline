10468622ee66b287bb1f2377f2bf49180cad83ec
Tran Van Khanh <midnight.tvk.96@gmail.com>
2025-04-16 10:13:21 +0700

    update giao dien chinh bang css va js

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.querySelectorAll('.accordion-button').forEach(button => {
+    button.addEventListener('click', () => {
+        const accordion = button.parentElement;
+        accordion.classList.toggle('active');
+    });
+});
